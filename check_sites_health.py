38dc729478f399af7cc3f0bdefb100f85e57f660
mixxuss <mixxuss@gmail.com>
2017-02-09 11:35:38 +0300

    Fixed is domain expires func and output

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,6 +1,7 @@
 import requests
 import argparse
 import time
+import datetime
 
 def create_parser():
     parser = argparse.ArgumentParser()
@@ -30,8 +31,13 @@ def get_domain_expiration_date(domain_name):
     return response.json()['paid']
 
 
-def is_domain_expires_in_month(expire_date):
-    pass
+def is_domain_expires_in_month(expire_date_str):
+    expire_date = datetime.datetime.strptime(expire_date_str, '%d.%m.%Y')
+    plus_month_date = datetime.datetime.today() + datetime.timedelta(days=30)
+    if expire_date > plus_month_date:
+        return False
+    else:
+        return True
 
 if __name__ == '__main__':
     parsed_args = create_parser()
@@ -42,7 +48,11 @@ if __name__ == '__main__':
         is_200 = is_server_respond_with_200(site)
         domain_expire = get_domain_expiration_date(site)
         if is_200:
-            print('Site {} is reachable by http, domain expires {}'.format(site,domain_expire))
+            print('Site {} is reachable by http'.format(site))
         else:
-            print('Site {} is not reachable by http, domain expires {}'.format(site, domain_expire))
+            print('Site {} is not reachable by http'.format(site))
+        if is_domain_expires_in_month(domain_expire):
+            print('Domain expires less than in month ({})'.format(domain_expire))
+        else:
+            print('Domain expires more than in month ({})'.format(domain_expire))
         time.sleep(delay_between_requests)
